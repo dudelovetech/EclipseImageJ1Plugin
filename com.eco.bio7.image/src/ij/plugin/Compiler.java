4427a586955752828689f21da82f653040a2d2df
elk <marcel.au@web.de>
2015-12-16 16:27:04 +0100

    Minor changes.

--- com.eco.bio7.image/src/ij/plugin/Compiler.java
+++ com.eco.bio7.image/src/ij/plugin/Compiler.java
@@ -139,7 +139,7 @@ public class Compiler implements PlugIn, FilenameFilter {
 		
 		IJ.showStatus("compiling "+path);
 		String classpath = getClassPath(path)+";"+binPath;
-		System.out.println(classpath);
+		//System.out.println(classpath);
 		
 		
 		Vector options = new Vector();
