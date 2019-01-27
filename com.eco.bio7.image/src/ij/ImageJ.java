9ec85ea1b940ec8565a8a359550d0f4042ecb656
marcel <marcel@bio7>
2019-01-27 19:46:16 +0100

    Disabled the status line (SWT) on MacOSX

--- com.eco.bio7.image/src/ij/ImageJ.java
+++ com.eco.bio7.image/src/ij/ImageJ.java
@@ -351,18 +351,14 @@ public class ImageJ extends Frame implements ActionListener, MouseListener, KeyL
 		Display display = PlatformUI.getWorkbench().getDisplay();
 		/* MacOSX only seems to work with async method! */
 		if (Util.getOS().equals("Mac")) {
-			display.asyncExec(new Runnable() {
-
-				public void run() {
-					if (CanvasView.getCanvas_view() != null) {
-						CanvasView.getCanvas_view().setstatusline(s);
-					}
-
-					if (customImageJView != null) {
-						customImageJView.setstatusline(s);
-					}
-				}
-			});
+			/*
+			 * display.asyncExec(new Runnable() {
+			 * 
+			 * public void run() { if (CanvasView.getCanvas_view() != null) {
+			 * CanvasView.getCanvas_view().setstatusline(s); }
+			 * 
+			 * if (customImageJView != null) { customImageJView.setstatusline(s); } } });
+			 */
 		} else {
 			display.syncExec(new Runnable() {
 
