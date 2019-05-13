2b390fcf286aa87d88246d75ea5dd281648c7259
elk <marcel.au@web.de>
2019-05-13 15:52:21 +0200

    Improve the centering of dialogs in a multi-monitor setup.

--- com.eco.bio7.image/src/ij/gui/GUI.java
+++ com.eco.bio7.image/src/ij/gui/GUI.java
@@ -1,4 +1,5 @@
 package ij.gui;
+
 import ij.*;
 import java.awt.*;
 import javax.swing.JComponent;
@@ -6,11 +7,19 @@ import javax.swing.JList;
 import javax.swing.JTable;
 import javax.swing.UIManager;
 
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Monitor;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+
+import com.eco.bio7.image.Util;
+
 /** This class consists of static GUI utility methods. */
 public class GUI {
 	private static final Font DEFAULT_FONT = new Font(Font.SANS_SERIF, Font.PLAIN, 12);
-	private static Color lightGray = new Color(240,240,240);
+	private static Color lightGray = new Color(240, 240, 240);
 	private static boolean isWindows8;
+	private static Point center;
 
 	static {
 		if (IJ.isWindows()) {
@@ -18,22 +27,67 @@ public class GUI {
 			isWindows8 = osname.contains("unknown") || osname.contains("8");
 		}
 	}
+        /*Calculate the central point of the monitor which contains the Bio7 shell!*/
+	public static Point getCenterPoint() {
+		Shell parentShell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
 
-	/** Positions the specified window in the center of the screen that contains target. */
+		Monitor[] monitors = parentShell.getDisplay().getMonitors();
+		Monitor activeMonitor = null;
+
+		org.eclipse.swt.graphics.Rectangle r = parentShell.getBounds();
+		for (int i = 0; i < monitors.length; i++) {
+			if (monitors[i].getBounds().contains(r.x, r.y)) {
+				activeMonitor = monitors[i];
+			}
+
+		}
+		if (activeMonitor == null) {
+			activeMonitor = parentShell.getDisplay().getPrimaryMonitor();
+		}
+		//System.out.println("The resolution of the active monitor is " + activeMonitor.getBounds().width + " x " + activeMonitor.getBounds().height);
+		// System.out.println(shellBounds);
+
+		org.eclipse.swt.graphics.Rectangle bounds = activeMonitor.getBounds();
+		return new Point(bounds.x + bounds.width / 2, bounds.y + bounds.height / 2);
+	}
+
+	/**
+	 * Positions the specified window in the center of the screen that contains
+	 * target (Bio7 shell).
+	 */
 	public static void center(Window win, Component target) {
 		if (win == null)
 			return;
-		Rectangle bounds = getMaxWindowBounds(target);
-		Dimension window = win.getSize();
-		if (window.width == 0)
-			return;
-		int left = bounds.x + Math.max(0, (bounds.width - window.width) / 2);
-		int top = bounds.y + Math.max(0, (bounds.height - window.height) / 4);
-		win.setLocation(left, top);
+		Display dis = Util.getDisplay();
+		dis.syncExec(new Runnable() {
+
+			public void run() {
+				center = getCenterPoint();
+			}
+		});
+		
+		Dimension window = win.getSize(); if (window.width == 0) return;
+		win.setLocation(center.x- window.width/2, center.y- window.height/2);
+		
 	}
-	
-	/** Positions the specified window in the center of the
-		 screen containing the "ImageJ" window. */
+
+	/**
+	 * Positions the specified window in the center of the screen that contains
+	 * target.
+	 */
+	/*
+	 * public static void center(Window win, Component target) { if (win == null)
+	 * return; Rectangle bounds = getMaxWindowBounds(target); Dimension window =
+	 * win.getSize(); if (window.width == 0) return; int left = bounds.x +
+	 * Math.max(0, (bounds.width - window.width) / 2); int top = bounds.y +
+	 * Math.max(0, (bounds.height - window.height) / 4); win.setLocation(left, top);
+	 * }
+	 */
+
+	/**
+	 * Positions the specified window in the center of the screen containing the
+	 * "ImageJ" window.
+	 */
 	public static void centerOnImageJScreen(Window win) {
 		center(win, IJ.getInstance());
 	}
@@ -41,7 +95,7 @@ public class GUI {
 	public static void center(Window win) {
 		center(win, win);
 	}
-	
+
 	private static java.util.List<GraphicsConfiguration> getScreenConfigs() {
 		java.util.ArrayList<GraphicsConfiguration> configs = new java.util.ArrayList<GraphicsConfiguration>();
 		for (GraphicsDevice device : GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()) {
@@ -49,16 +103,19 @@ public class GUI {
 		}
 		return configs;
 	}
-	
+
 	/**
 	 * Get maximum bounds for the screen that contains a given point.
-	 * @param point Coordinates of point.
-	 * @param accountForInsets Deduct the space taken up by menu and status bars, etc. (after point is found to be inside bonds)
-	 * @return Rectangle of bounds or <code>null</code> if point not inside of any screen.
+	 * 
+	 * @param point            Coordinates of point.
+	 * @param accountForInsets Deduct the space taken up by menu and status bars,
+	 *                         etc. (after point is found to be inside bonds)
+	 * @return Rectangle of bounds or <code>null</code> if point not inside of any
+	 *         screen.
 	 */
 	public static Rectangle getScreenBounds(Point point, boolean accountForInsets) {
 		if (GraphicsEnvironment.isHeadless())
-			return new Rectangle(0,0,0,0);
+			return new Rectangle(0, 0, 0, 0);
 		for (GraphicsConfiguration config : getScreenConfigs()) {
 			Rectangle bounds = config.getBounds();
 			if (bounds != null && bounds.contains(point)) {
@@ -66,37 +123,39 @@ public class GUI {
 				return shrinkByInsets(bounds, insets);
 			}
 		}
-		return null;		
+		return null;
 	}
-	
+
 	/**
 	 * Get maximum bounds for the screen that contains a given component.
-	 * @param component An AWT component located on the desired screen.
-	 * If <code>null</code> is provided, the default screen is used.
-	 * @param accountForInsets Deduct the space taken up by menu and status bars, etc.
+	 * 
+	 * @param component        An AWT component located on the desired screen. If
+	 *                         <code>null</code> is provided, the default screen is
+	 *                         used.
+	 * @param accountForInsets Deduct the space taken up by menu and status bars,
+	 *                         etc.
 	 * @return Rectangle of bounds.
-	 */	
+	 */
 	public static Rectangle getScreenBounds(Component component, boolean accountForInsets) {
 		if (GraphicsEnvironment.isHeadless())
-			return new Rectangle(0,0,0,0);
-		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();		
-		GraphicsConfiguration gc = component == null ? ge.getDefaultScreenDevice().getDefaultConfiguration() :
-													   component.getGraphicsConfiguration();   
+			return new Rectangle(0, 0, 0, 0);
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsConfiguration gc = component == null ? ge.getDefaultScreenDevice().getDefaultConfiguration() : component.getGraphicsConfiguration();
 		Insets insets = accountForInsets ? Toolkit.getDefaultToolkit().getScreenInsets(gc) : null;
 		return shrinkByInsets(gc.getBounds(), insets);
 	}
 
 	public static Rectangle getScreenBounds(Point point) {
 		return getScreenBounds(point, false);
-	}		
+	}
 
 	public static Rectangle getScreenBounds(Component component) {
 		return getScreenBounds(component, false);
-	}			
+	}
 
 	public static Rectangle getScreenBounds() {
-		return getScreenBounds((Component)null);
-	}			
+		return getScreenBounds((Component) null);
+	}
 
 	public static Rectangle getMaxWindowBounds(Point point) {
 		return getScreenBounds(point, true);
@@ -105,21 +164,22 @@ public class GUI {
 	public static Rectangle getMaxWindowBounds(Component component) {
 		return getScreenBounds(component, true);
 	}
-	
+
 	public static Rectangle getMaxWindowBounds() {
-		return getMaxWindowBounds((Component)null);
+		return getMaxWindowBounds((Component) null);
 	}
-	
+
 	private static Rectangle shrinkByInsets(Rectangle bounds, Insets insets) {
 		Rectangle shrunk = new Rectangle(bounds);
-		if (insets == null) return shrunk; 
+		if (insets == null)
+			return shrunk;
 		shrunk.x += insets.left;
 		shrunk.y += insets.top;
 		shrunk.width -= insets.left + insets.right;
 		shrunk.height -= insets.top + insets.bottom;
 		return shrunk;
 	}
-	
+
 	public static Rectangle getZeroBasedMaxBounds() {
 		for (GraphicsConfiguration config : getScreenConfigs()) {
 			Rectangle bounds = config.getBounds();
@@ -128,7 +188,7 @@ public class GUI {
 		}
 		return null;
 	}
-	
+
 	public static Rectangle getUnionOfBounds() {
 		Rectangle unionOfBounds = new Rectangle();
 		for (GraphicsConfiguration config : getScreenConfigs()) {
@@ -136,46 +196,48 @@ public class GUI {
 		}
 		return unionOfBounds;
 	}
-	
-    static private Frame frame;
-    
-    /** Creates a white AWT Image image of the specified size. */
-    public static Image createBlankImage(int width, int height) {
-        if (width==0 || height==0)
-            throw new IllegalArgumentException("");
-		if (frame==null) {
+
+	static private Frame frame;
+
+	/** Creates a white AWT Image image of the specified size. */
+	public static Image createBlankImage(int width, int height) {
+		if (width == 0 || height == 0)
+			throw new IllegalArgumentException("");
+		if (frame == null) {
 			frame = new Frame();
 			frame.pack();
 			frame.setBackground(Color.white);
 		}
-        Image img = frame.createImage(width, height);
-        return img;
-    }
-    
-    /** Lightens overly dark scrollbar background on Windows 8. */
-    public static void fix(Scrollbar sb) {
-    }
-    
-    public static boolean showCompositeAdvisory(ImagePlus imp, String title) {
-    	if (imp==null || imp.getCompositeMode()!=IJ.COMPOSITE || imp.getNChannels()==1 || IJ.macroRunning())
-    		return true;
-    	String msg = "Channel "+imp.getC()+" of this color composite image will be processed.";
+		Image img = frame.createImage(width, height);
+		return img;
+	}
+
+	/** Lightens overly dark scrollbar background on Windows 8. */
+	public static void fix(Scrollbar sb) {
+	}
+
+	public static boolean showCompositeAdvisory(ImagePlus imp, String title) {
+		if (imp == null || imp.getCompositeMode() != IJ.COMPOSITE || imp.getNChannels() == 1 || IJ.macroRunning())
+			return true;
+		String msg = "Channel " + imp.getC() + " of this color composite image will be processed.";
 		GenericDialog gd = new GenericDialog(title);
 		gd.addMessage(msg);
 		gd.showDialog();
 		return !gd.wasCanceled();
 	}
-	
+
 	/**
 	 * Scales an AWT component according to {@link Prefs#getGuiScale()}.
-	 * @param component the AWT component to be scaled. If a container, scaling is applied to all its child components
+	 * 
+	 * @param component the AWT component to be scaled. If a container, scaling is
+	 *                  applied to all its child components
 	 */
 	public static void scale(final Component component) {
-		final float scale = (float)Prefs.getGuiScale();
-		if (scale==1f)
+		final float scale = (float) Prefs.getGuiScale();
+		if (scale == 1f)
 			return;
 		if (component instanceof Container)
-			scaleComponents((Container)component, scale);
+			scaleComponents((Container) component, scale);
 		else
 			scaleComponent(component, scale);
 	}
@@ -193,21 +255,21 @@ public class GUI {
 		Font font = component.getFont();
 		if (font == null)
 			font = DEFAULT_FONT;
-		font = font.deriveFont(scale*font.getSize());
+		font = font.deriveFont(scale * font.getSize());
 		component.setFont(font);
 	}
 
 	public static void scalePopupMenu(final PopupMenu popup) {
 		final float scale = (float) Prefs.getGuiScale();
-		if (scale==1f)
+		if (scale == 1f)
 			return;
 		Font font = popup.getFont();
 		if (font == null)
 			font = DEFAULT_FONT;
-		font = font.deriveFont(scale*font.getSize());
+		font = font.deriveFont(scale * font.getSize());
 		popup.setFont(font);
 	}
-	
+
 	/**
 	 * Tries to detect if a Swing component is unscaled and scales it it according
 	 * to {@link #getGuiScale()}.
